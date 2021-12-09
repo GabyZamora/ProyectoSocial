68840bab3122fd3068c2baaf64bb35ebc73f81ae
gabyzamora <gabriela.zamora99@hotmail.com>
2021-12-09 16:37:46 -0600

    nav

--- presentacion/info.php
+++ presentacion/info.php
@@ -1,3 +1,6 @@
+<?php
+include('nav.php');
+?>
 <link href="https://fonts.googleapis.com/css2?family=Bitter&family=Montserrat&family=Quattrocento+Sans:wght@700&display=swap" rel="stylesheet"> 
 <link rel="stylesheet" href="css/iconfont/material-icons.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -11,36 +14,13 @@
 <script src="js/jquery-3.4.0.min.js"></script>
 <script src="js/bootstrap-4.3.1.min.js"></script>
 </head>
-<body>
-    <nav class="nav navbar-default navbar-expand">
-        <div class="content">
-            <a href="index.php"><p>Productos Naturales</p>
-            <p style="text-align: center; color:#3fc4ac;">Aura</p></a>
-            <img src="images/logoaura2.jpg">
-            <div class="navbar d-flex col"> 
-                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle navbar-toggler ml-auto"> 
-                <span class="navbar-toggler-icon"></span> 
-                <span class="icon-bar"></span> 
-                <span class="icon-bar"></span> 
-                <span class="icon-bar"></span> 
-                </button> 
-            </div>
-            <div id="navbarCollapse" class="collapse navbar-collapse justify-content-start navbar-right">
-                <ul>
-                    <li><a href="#">Sobre Nosotros</a></li>
-                    <li><a href="#">Productos</a></li>
-                    <li><a href="#">Servicios</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
     <div class="info">
-        <p style="margin-top: 50px; margin-left: 30px;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste quod atque  <br/>
-        assumenda suscipit commodi harum similique enim libero laboriosam dignissimos, non nihil dolorem  <br/>
-        consequatur illo, accusamus, quae odio quod possimus, officia nam numquam minima pariatur suscipit  <br/>
-        obcaecati vero dolorum aspernatur? mollitia culpa dicta molestias magni. Lorem ipsum dolor sit amet  <br/>
-        consectetur adipisicing elit. Aut tempora iste nobis Iste, necessitatibus amet! Lorem, ipsum dolor sit  <br/>
-        amet consectetur adipisicing elit. Distinctio expedita pariatur voluptate ducimus quaerat, enim doloribus? <br/>
+        <p style="margin-top: 50px; margin-left: 10px;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste quod atque 
+        assumenda suscipit commodi harum similique enim libero laboriosam dignissimos, non nihil dolorem  
+        consequatur illo, accusamus, quae odio quod possimus, officia nam numquam minima pariatur suscipit  
+        obcaecati vero dolorum aspernatur? mollitia culpa dicta molestias magni. Lorem ipsum dolor sit amet  
+        consectetur adipisicing elit. Aut tempora iste nobis Iste, necessitatibus amet! Lorem, ipsum dolor sit  
+        amet consectetur adipisicing elit. Distinctio expedita pariatur voluptate ducimus quaerat, enim doloribus?
         Atque laborum in repudiandae aliquam aliquid, alias nobis excepturi est deleniti eveniet earum hic.</p>
         <img style="margin-top: 2px; margin-left: 800px;" src="images/producto2.jpg">
     </div>
